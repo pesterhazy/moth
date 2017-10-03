f27c2dc83ea678e527abf2e4c491f96fed5f3dee
Paulus Esterhazy <pesterhazy@gmail.com>
2017-10-03 11:04:24 +0200

    WIP

--- main/lib/moth/http_provider.py
+++ main/lib/moth/http_provider.py
@@ -3,13 +3,22 @@ import requests
 from clint.textui import progress
 
 
+def process_progress(f, r, chunk_size, content_length):
+    total_length = int(content_length)
+    for chunk in progress.bar(r.iter_content(chunk_size=chunk_size),
+                              expected_size=(total_length/chunk_size) + 1):
+        if chunk:
+            f.write(chunk)
+
+
+def process(f, r, chunk_size):
+    for chunk in r.iter_content(chunk_size=chunk_size):
+        f.write(chunk)
+
 
 class HTTPProvider:
     def __init__(self, url):
         self.url = url
-
-    def upload(self, frm, to):
-        raise Exception("HTTPProvider cannot be used to upload objects")
 
     def download(self, frm, to):
         chunk_size = 1024
@@ -19,22 +28,14 @@ class HTTPProvider:
         with open(to, 'wb') as f:
             content_length = r.headers.get('content-length')
 
-            if content_length:
-                total_length = int(content_length)
-                for chunk in progress.bar(r.iter_content(chunk_size=chunk_size),
-                                          expected_size=(total_length/chunk_size) + 1):
-                    if chunk:
-                        f.write(chunk)
+            if content_length and not f.isatty():
+                process_progress(f=f, r=r, content_length=content_length,
+                                 chunk_size=chunk_size)
             else:
-                for chunk in r.iter_content(chunk_size=chunk_size):
-                    f.write(chunk)
+                process(f=f, r=r, chunk_size=chunk_size)
 
     def put(self, input_file):
-        sha = util.hash_file(input_file)
-        target_path = util.pjoin(self.url, "db", sha[0:3], sha, "contents")
-        self.upload(input_file, target_path)
-
-        return sha
+        raise Exception("HTTPProvider cannot be used to upload objects")
 
     def get(self, sha, output_file):
         target_path = util.pjoin(self.url, "db", sha[0:3], sha, "contents")
