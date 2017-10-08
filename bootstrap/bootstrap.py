f66c0df70fe95dcb6d2ebc3cdb69366a507f895d
Paulus Esterhazy <pesterhazy@gmail.com>
2017-10-08 22:17:47 +0200

    [WIP] Bootstrap to global cache folder (#14)
    Bootstrap to global cache folder

---
+++ bootstrap/bootstrap.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python2
+
+import errno
+import os
+import sys
+import urllib2
+import hashlib
+
+MAIN_SHA = "SHA_GOES_HERE"
+
+
+def get_main_sha():
+    if MAIN_SHA == "SHA_GOES_HERE":
+        return os.environ["MOTH_VERSION"]
+    else:
+        return MAIN_SHA
+
+
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc:  # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
+
+
+def cache_base_path():
+    return os.path.expanduser("~/.cache/moth")
+
+
+spinner_images = ["[    ]",
+                  "[=   ]",
+                  "[==  ]",
+                  "[=== ]",
+                  "[ ===]",
+                  "[  ==]",
+                  "[   =]",
+                  "[    ]",
+                  "[   =]",
+                  "[  ==]",
+                  "[ ===]",
+                  "[====]",
+                  "[=== ]",
+                  "[==  ]",
+                  "[=   ]"]
+
+
+def download(url, sha, target_file):
+    url_file = urllib2.urlopen(url)
+
+    hasher = hashlib.sha1()
+
+
+    mkdir_p(os.path.dirname(target_file))
+
+    with open(target_file, "w") as out:
+        n_chunks = 0
+        chunk_size = 1024 * 64
+        image = spinner_images[len(spinner_images) - 1]
+        sys.stderr.write(image)
+        sys.stderr.flush()
+        while 1:
+            data = url_file.read(chunk_size)
+            if not data:
+                break
+            hasher.update(data)
+            out.write(data)
+            sys.stderr.write("\b" * len(spinner_images[0]))
+            image = spinner_images[n_chunks % len(spinner_images)]
+            sys.stderr.write(image)
+            sys.stderr.flush()
+            n_chunks += 1
+
+    sys.stderr.write("\b" * (len(spinner_images[0])))
+    sys.stderr.write(" " * (len(spinner_images[0])))
+    sys.stderr.write("\b" * (len(spinner_images[0])))
+    sys.stderr.flush()
+
+    downloaded_sha = hasher.hexdigest()
+
+    if downloaded_sha != sha:
+        raise Exception("Downloaded sha " + downloaded_sha +
+                        " does not match expected value " + sha)
+
+
+def bootstrap(sha, target_file):
+    if os.environ.get("MOTH_BOOTSTRAP_REPO") is not None:
+        base_url = os.environ.get("MOTH_BOOTSTRAP_REPO")
+        url = base_url + "/db/" + sha[0:3] + "/" + sha + "/contents"
+    else:
+        url = "https://github.com/pesterhazy/moth/releases/download/r" + \
+            get_main_sha() + "/moth_release.zip"
+
+    sys.stderr.write("moth: bootstrapping version " + sha + ": ")
+    sys.stderr.flush()
+
+    download(url, sha, target_file)
+
+    sys.stderr.write("done.\n")
+
+
+def get_zip_path(sha):
+    return os.path.join(cache_base_path(), "binary", sha)
+
+
+def start():
+    ZIP_PATH = get_zip_path(get_main_sha())
+
+    if os.path.exists(ZIP_PATH):
+        sys.path.insert(0, ZIP_PATH)
+
+    try:
+        import moth.main
+    except ImportError:
+        bootstrap(get_main_sha(), ZIP_PATH)
+
+        if os.path.exists(ZIP_PATH):
+            sys.path.insert(0, ZIP_PATH)
+
+        import moth.main
+
+    moth.main.run(os.path.dirname(__file__))
+
+
+start()
